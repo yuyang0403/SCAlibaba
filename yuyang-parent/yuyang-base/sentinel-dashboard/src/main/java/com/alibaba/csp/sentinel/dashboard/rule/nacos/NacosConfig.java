84cf1f0cadbd8b5a5725a2b632b52d01906c9e0d
yuyang <yuyang01@sinoiov.com>
2020-06-04 10:48:59 +0800

    sentinel dashborad实现修改限流、降级规则推送到数据源nacos中

---
+++ yuyang-parent/yuyang-base/sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/NacosConfig.java
@@ -0,0 +1,47 @@
+package com.alibaba.csp.sentinel.dashboard.rule.nacos;
+
+
+import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.DegradeRuleEntity;
+import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.FlowRuleEntity;
+import com.alibaba.csp.sentinel.datasource.Converter;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.nacos.api.PropertyKeyConst;
+import com.alibaba.nacos.api.config.ConfigFactory;
+import com.alibaba.nacos.api.config.ConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.Properties;
+
+@Configuration
+public class NacosConfig {
+
+    @Autowired
+    private NacosConfigProperties nacosConfigProperties;
+
+
+    @Bean
+    public Converter<List<FlowRuleEntity>, String> flowRuleEntityEncoder() {
+        return JSON::toJSONString;
+    }
+
+    @Bean
+    public Converter<List<DegradeRuleEntity>, String> degradeRuleEntityEncoder() {
+        return JSON::toJSONString;
+    }
+
+    @Bean
+    public Converter<String, List<FlowRuleEntity>> flowRuleEntityDecoder() {
+        return s -> JSON.parseArray(s, FlowRuleEntity.class);
+    }
+
+    @Bean
+    public ConfigService nacosConfigService() throws Exception {
+        Properties properties = new Properties();
+        properties.put(PropertyKeyConst.SERVER_ADDR, nacosConfigProperties.getServerAddr());
+        properties.put(PropertyKeyConst.NAMESPACE, nacosConfigProperties.getNamespace());
+        return ConfigFactory.createConfigService(properties);
+    }
+}
